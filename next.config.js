4cf127c3c5caf1952922c4984629334da4ab5fe8
VallariAg <val.agl002@gmail.com>
2021-11-23 16:26:55 +0530

    Add webpack in next.config to use one polyfill
    Before commit: infinite errors on IE
    After commit: infinite error gone. Just server error on IE for Fictoan's Element.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: function (config) {
+    const originalEntry = config.entry;
+
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./polyfills.js")
+      ) {
+        entries["main.js"].unshift("./polyfills.js");
+      }
+      return entries;
+    };
+    return config;
+  },
 }
